6666bf15f2a5ca2d200bb257f85339f7b9598bc3
carlos22ivan <carlos22ivan@gmail.com>
2019-08-04 17:55:47 -0500

    fix

---
+++ start.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+source ./.env
+
+usermod -u ${USER_ID} apache
+groupmod -g ${GROUP_ID} apache
+
+MOODLE_CONFIG=/var/www/html/moodle/config.php
+
+if test -f "$MOODLE_CONFIG"; then
+
+    sed -i "/wwwroot/c\$CFG->wwwroot   = '${MOODLE_URL}';"      $MOODLE_CONFIG
+    sed -i "/dbhost/c\$CFG->dbhost    = '${DATABASE_URL}';"     $MOODLE_CONFIG
+    sed -i "/dbport/c\  'dbport' => '${DATABASE_PORT}',"        $MOODLE_CONFIG
+    sed -i "/dbtype/c\$CFG->dbtype    = '${DATABASE_TYPE}';"    $MOODLE_CONFIG
+    sed -i "/dbname/c\$CFG->dbname    = '${DATABASE_NAME}';"    $MOODLE_CONFIG
+    sed -i "/dbuser/c\$CFG->dbuser    = '${DATABASE_USER}';"    $MOODLE_CONFIG
+    sed -i "/dbpass/c\$CFG->dbpass    = '${DATABASE_PASS}';"    $MOODLE_CONFIG
+fi
+
+echo ">    successful deployment"
+echo ">    moodle run in ${MOODLE_NETWORK}:${MOODLE_PORT}"
+
+/usr/sbin/apachectl -DFOREGROUND
