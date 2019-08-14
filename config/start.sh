466290d21235bc264ec1566a14e79390891d95da
carlos22ivan <carlos22ivan@gmail.com>
2019-08-13 22:36:25 -0500

    fix php max file size

--- config/start.sh
+++ config/start.sh
@@ -6,6 +6,7 @@ usermod -u ${USER_ID} apache
 groupmod -g ${GROUP_ID} apache
 
 MOODLE_CONFIG=/var/www/html/moodle/config.php
+PHP_INI=/etc/php.ini
 
 if test -f "$MOODLE_CONFIG"; then
 
@@ -20,6 +21,10 @@ if test -f "$MOODLE_CONFIG"; then
     sed -i "/proxy/c\$CFG->sslproxy = ${MOODLE_SSL};"           $MOODLE_CONFIG
     
 fi
+
+sed -i "/post_max_size/c\post_max_size = ${MOODLE_MAX_UPLOAD_SIZE}" $PHP_INI
+sed -i "/upload_max_filesize/c\upload_max_filesize = ${MOODLE_MAX_UPLOAD_SIZE}" $PHP_INI
+sed -i "/max_execution_time/c\max_execution_time = 600" $PHP_INI
 
 echo ">    successful deployment"
 echo ">    moodle run in ${MOODLE_NETWORK}:${MOODLE_PORT}"
